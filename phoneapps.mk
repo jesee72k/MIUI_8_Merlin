2c3f1b50b0055c443fdac5d91601ea096cebb8d9
Henrique Silva <jhenrique09.mcz@hotmail.com>
2016-12-01 13:15:52 -0300

    Default files added

---
+++ phoneapps.mk
@@ -0,0 +1,37 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	Bluetooth \
+	BluetoothExt \
+	BluetoothMidiService \
+	CertInstaller \
+	KeyChain \
+	PacProcessor \
+	PicoTts \
+	PrintSpooler \
+	Stk \
+	telresources \
+	TimeService \
+	WAPPushManager \
+ 	MiuiVideoPlayer
+
+private-phone-priv-apps := \
+	AudioFX \
+	BackupRestoreConfirmation \
+	CarrierConfig \
+	CellBroadcastReceiver \
+	CMAudioService \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FMRadio \
+	FusedLocation \
+	InputDevices \
+	ProxyHandler \
+	qcrilmsgtunnel \
+	SharedStorageBackup \
+	Shell \
+	StatementService \
+ 	TeleService \
+ 	CMActions \
+ 	Snap
